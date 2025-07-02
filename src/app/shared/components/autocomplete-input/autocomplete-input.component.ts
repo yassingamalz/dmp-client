6c69eab78c182222e11986906e2bd780647cd779
ygsohim <ygsohim@ferrari.com>
2025-07-02 14:29:37 +0200

    feat: add shared components for header, toast, modal and autocomplete

---
+++ src/app/shared/components/autocomplete-input/autocomplete-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autocomplete-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './autocomplete-input.component.html',
+  styleUrl: './autocomplete-input.component.scss'
+})
+export class AutocompleteInputComponent {
+
+}
